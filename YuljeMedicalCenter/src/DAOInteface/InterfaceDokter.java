cf81d3428014a3ba4090c909947cd05d49d58ebe
Rafli <rafliyulistiawan@gmail.com>
2023-05-11 23:23:52 +0700

    fixed

---
+++ YuljeMedicalCenter/src/DAOInteface/InterfaceDokter.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package DAOInteface;
+
+import java.util.List;
+import model.Dokter;
+
+/**
+ *
+ * @author Yudha
+ */
+public interface InterfaceDokter {
+    public List<Dokter> getAll();
+    
+}
