68be7155592fbe23d1674ce32a505d14ddd19bf9
Rafli <rafliyulistiawan@gmail.com>
2023-05-12 21:20:12 +0700

    fix tampilan, add tampilan for antrian

--- YuljeMedicalCenter/src/view/ViewMain.java
+++ YuljeMedicalCenter/src/view/ViewMain.java
@@ -44,6 +44,7 @@ public class ViewMain extends javax.swing.JFrame {
     jButton1.setForeground(new java.awt.Color(255, 255, 255));
     jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/doctor.png"))); // NOI18N
     jButton1.setText("Informasi Dokter");
+    jButton1.setBorder(null);
     jButton1.setIconTextGap(8);
     jButton1.addActionListener(new java.awt.event.ActionListener() {
       public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -56,6 +57,7 @@ public class ViewMain extends javax.swing.JFrame {
     jButton2.setForeground(new java.awt.Color(255, 255, 255));
     jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/people-queue-24-regular.png"))); // NOI18N
     jButton2.setText("Informasi Antrian");
+    jButton2.setBorder(null);
     jButton2.setIconTextGap(8);
     jButton2.addActionListener(new java.awt.event.ActionListener() {
       public void actionPerformed(java.awt.event.ActionEvent evt) {
